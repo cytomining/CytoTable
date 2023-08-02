03ef64fc1c2d5247963e552b183dff2afe3e97ba
d33bs <dave.bunten@cuanschutz.edu>
2023-08-02 13:54:05 -0600

    global names

--- tests/data/cytominer-database/Cell-Health/shrink-SQ00014613.sqlite-for-testing.py
+++ tests/data/cytominer-database/Cell-Health/shrink-SQ00014613.sqlite-for-testing.py
@@ -9,19 +9,19 @@ Cell_Health_-_Cell_Painting_Single_Cell_Profiles/9995672?file=18506036
 import shutil
 import sqlite3
 
-sqlite_source = "SQ00014613.sqlite"
-sqlite_target = "test-SQ00014613.sqlite"
+SQLITE_SOURCE = "SQ00014613.sqlite"
+SQLITE_TARGET = "test-SQ00014613.sqlite"
 
 # note: we presume the pre-existence of SQ00014613.sqlite
 # from an earlier download outside of this python work.
-shutil.copy(sqlite_source, sqlite_target)
+shutil.copy(SQLITE_SOURCE, SQLITE_TARGET)
 
-with sqlite3.connect(sqlite_target) as conn:
+with sqlite3.connect(SQLITE_TARGET) as conn:
     # delete data except that related to two tablenumbers
     conn.execute(
         """
-        DELETE FROM Image 
-        WHERE TableNumber NOT IN 
+        DELETE FROM Image
+        WHERE TableNumber NOT IN
         ('88ac13033d9baf49fda78c3458bef89e',
         '1e5d8facac7508cfd4086f3e3e950182')
         """
@@ -30,7 +30,7 @@ with sqlite3.connect(sqlite_target) as conn:
     for table in ["Cells", "Nuclei", "Cytoplasm"]:
         conn.execute(
             f"""
-            DELETE FROM {table} 
+            DELETE FROM {table}
             WHERE TableNumber NOT IN (SELECT TableNumber FROM Image)
             OR ObjectNumber > 6
             """
