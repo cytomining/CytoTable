088aafae280af506e24c18e71727e8832ac61d94
d33bs <dave.bunten@cuanschutz.edu>
2023-09-26 16:32:37 -0600

    move to new exception type no dfk for config check

--- cytotable/utils.py
+++ cytotable/utils.py
@@ -14,7 +14,7 @@ from cloudpathlib import AnyPath, CloudPath
 from cloudpathlib.exceptions import InvalidPrefixError
 from parsl.app.app import AppBase
 from parsl.config import Config
-from parsl.errors import ConfigurationError
+from parsl.errors import NoDataFlowKernelError
 from parsl.executors import HighThroughputExecutor
 
 logger = logging.getLogger(__name__)
@@ -108,10 +108,10 @@ def _parsl_loaded() -> bool:
     try:
         # try to reference Parsl dataflowkernel
         parsl.dfk()
-    except ConfigurationError as pce:
+    except NoDataFlowKernelError as ndfke:
         # if we detect a Parsl ConfigurationError that states we need to load config
         # return false to indicate parsl config has not yet been loaded.
-        if pce.args[0] == "Must first load config":
+        if ndfke.args[0] == "Must first load config":
             return False
 
         # otherwise we raise other ConfigurationError's
