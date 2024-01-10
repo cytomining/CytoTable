d793e2a9d177d29ffbd170e702b39d8a68d0bc49
d33bs <dave.bunten@cuanschutz.edu>
2024-01-09 17:17:43 -0700

    add module for shrinking in carta dataset

---
+++ tests/data/in-carta/colas-lab/shrink_colas-lab_data_for_tests.py
@@ -0,0 +1,54 @@
+"""
+Shrink datasets from Colas Lab from IN Carta provided as collection of CSV's.
+
+Note: built to be run from CytoTable poetry dev environment from project base, e.g.:
+`poetry run python tests/data/in-carta/colas-lab/shrink_colas-lab_data_for_tests.py`
+"""
+
+import pathlib
+from typing import Dict, List, Union
+
+import duckdb
+import pyarrow as pa
+from pyarrow import csv
+
+# set a path for local data source
+source_data_dir = "tests/data/in-carta/colas-lab/data"
+target_data_dir = "tests/data/in-carta/colas-lab"
+
+# build a collection of schema
+schema_collection: List[Dict[str, Union[str, pa.Schema]]] = []
+for data_file in pathlib.Path(source_data_dir).rglob("*.csv"):
+    with duckdb.connect() as ddb:
+        # read the csv file as a pyarrow table and extract detected schema
+        table = ddb.execute(
+            f"""
+            SELECT *
+            FROM read_csv_auto('{data_file}')
+            """
+        ).arrow()
+        schema_collection.append({"file": data_file, "schema": table.schema})
+
+# determine if the schema are exactly alike
+for schema in schema_collection:
+    for schema_to_compare in schema_collection:
+        # compare every schema to all others
+        if schema["file"] != schema_to_compare["file"]:
+            if not schema["schema"].equals(schema_to_compare["schema"]):
+                raise Exception("Inequal schema detected.")
+
+
+for data_file in pathlib.Path(source_data_dir).rglob("*.csv"):
+    with duckdb.connect() as ddb:
+        # read the csv file as a pyarrow table append to list for later use
+        csv.write_csv(
+            data=ddb.execute(
+                f"""
+                SELECT *
+                FROM read_csv_auto('{data_file}') as data_file
+                WHERE data_file."OBJECT ID" in (1,2,3)
+                AND data_file."ROW" in ('C', 'D')
+                """
+            ).arrow(),
+            output_file=f"{target_data_dir}/test-{pathlib.Path(data_file).name}",
+        )
