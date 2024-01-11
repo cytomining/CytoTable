75c5822c9cdf62f7174618da44a5bd32a67d0b80
Dave Bunten <dave.bunten@cuanschutz.edu>
2024-01-11 05:43:46 -0700

    Add IN Carta test data from Colas Lab (#141)
    * add module for shrinking in carta dataset
    
    * add test datasets created from module
    
    * add citation reference
    
    * linting
    
    * linting and module snakecase
    
    * linting
    
    * remove typing imports
    
    * remove unused imports remove file
    
    * clarity in citation file
    
    * linting
    
    * simplify and add comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Gregory Way <gregory.way@gmail.com>
    
    * add names of data authors; adjust notes content
    
    * add comments to further clarify the operations
    
    * use shortened filename
    
    * rename example data files
    
    ---------
    
    Co-authored-by: Gregory Way <gregory.way@gmail.com>

---
+++ tests/data/in-carta/colas-lab/shrink_colas_lab_data_for_tests.py
@@ -0,0 +1,65 @@
+"""
+Shrink datasets from Colas Lab from IN Carta provided as collection of CSV's.
+
+Note: built to be run from CytoTable poetry dev environment from project base, e.g.:
+`poetry run python tests/data/in-carta/colas-lab/shrink_colas_lab_data_for_tests.py`
+"""
+
+import pathlib
+
+import duckdb
+from pyarrow import csv
+
+# set a path for local and target data dir
+SOURCE_DATA_DIR = "tests/data/in-carta/colas-lab/data"
+TARGET_DATA_DIR = "tests/data/in-carta/colas-lab"
+
+# build a collection of schema
+schema_collection = []
+for data_file in pathlib.Path(SOURCE_DATA_DIR).rglob("*.csv"):
+    with duckdb.connect() as ddb:
+        # read the csv file as a pyarrow table and extract detected schema
+        schema_collection.append(
+            {
+                "file": data_file,
+                "schema": ddb.execute(
+                    f"""
+                    SELECT *
+                    FROM read_csv_auto('{data_file}')
+                    """
+                )
+                .arrow()
+                .schema,
+            }
+        )
+
+# determine if the schema are exactly alike
+for schema in schema_collection:
+    for schema_to_compare in schema_collection:
+        # compare every schema to all others
+        if schema["file"] != schema_to_compare["file"]:
+            if not schema["schema"].equals(schema_to_compare["schema"]):
+                # if we detect that the schema are inequal, raise an exception
+                raise TypeError("Inequal schema detected.")
+
+
+for idx, data_file in enumerate(pathlib.Path(SOURCE_DATA_DIR).rglob("*.csv")):
+    with duckdb.connect() as ddb:
+        # Read the csv file with SQL-based filters
+        # as a pyarrow table then output to a new and
+        # smaller csv for testing purposes.
+        csv.write_csv(
+            # we use duckdb to filter the original dataset in SQL
+            data=ddb.execute(
+                f"""
+                SELECT *
+                FROM read_csv_auto('{data_file}') as data_file
+                /* select only the first three objects to limit the dataset */
+                WHERE data_file."OBJECT ID" in (1,2,3)
+                /* select rows C and D to limit the dataset */
+                AND data_file."ROW" in ('C', 'D')
+                """
+            ).arrow(),
+            # output the filtered data as a CSV to a new location
+            output_file=f"{TARGET_DATA_DIR}/test-in-carta-{idx}.csv",
+        )
