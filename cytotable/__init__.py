7865a76c6afb6ff489708112de26b545afc7b54e
d33bs <ekgto445@gmail.com>
2025-05-28 16:28:19 -0600

    avoid warnings from google sub-dependencies

--- cytotable/__init__.py
+++ cytotable/__init__.py
@@ -5,7 +5,6 @@ __init__.py for cytotable
 # note: version data is maintained by poetry-dynamic-versioning (do not edit)
 __version__ = "0.0.0"
 
-
 # filter warnings about pkg_resources deprecation
 # note: these stem from cloudpathlib google cloud
 # dependencies.
@@ -14,8 +13,7 @@ import warnings
 warnings.filterwarnings(
     "ignore",
     message=(
-        "pkg_resources is deprecated as an API."
-        "See https://setuptools.pypa.io/en/latest/pkg_resources.html."
+        ".*pkg_resources is deprecated as an API.*"
     ),
     category=UserWarning,
     module="google_crc32c.__config__",
