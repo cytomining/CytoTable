23e0f96e520f01ce1444bc2cb306bbfaaab2bcd5
d33bs <ekgto445@gmail.com>
2025-09-02 13:13:46 -0600

    updates based on pull request review
    Co-Authored-By: Gregory Way <gregory.way@gmail.com>

--- docs/source/examples/cytotable_from_the_cloud.py
+++ docs/source/examples/cytotable_from_the_cloud.py
@@ -3,8 +3,8 @@
 #   jupytext:
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
+#       format_name: percent
+#       format_version: '1.3'
 #       jupytext_version: 1.17.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
@@ -12,22 +12,23 @@
 #     name: python3
 # ---
 
+# %% [markdown]
 # # CytoTable from the cloud (using cloud-based data sources)
 #
-# ![image.png](attachment:57b05fac-35c8-4648-a6c9-a6bba89a22bf.png)
+# ![Image showing feature data from the cloud being read by CytoTable and exported to a CytoTable file afterwards.](../_static/features_to_cytotable_cloud.png)
 #
-# __Figure 1.__ _CytoTable is capable of reading feature data from cloud-based locations such as AWS S3._
+# __Figure 1.__ _CytoTable is capable of reading data from cloud-based locations such as AWS S3._
 #
 # This notebook includes a quick demonstration of CytoTable with cloud-based data sources.
 # For a more general overview of using CytoTable and the concepts behind the work please see: [CytoTable mise en place (general overview)](https://cytomining.github.io/CytoTable/examples/cytotable_mise_en_place_general_overview.html)
 
-# +
+# %%
 import pathlib
 from collections import Counter
+from cloudpathlib import S3Client, CloudPath
 
 import pandas as pd
 import pyarrow.parquet as pq
-from cloudpathlib import CloudPath, S3Client
 from IPython.display import Image, display
 from PIL import Image
 
@@ -36,20 +37,23 @@ import cytotable
 # setup variables for use throughout the notebook
 source_path = "s3://cellpainting-gallery/cpg0000-jump-pilot/source_4/workspace/analysis/2020_11_04_CPJUMP1/BR00116991/analysis/BR00116991-A01-1"
 dest_path = "./cloud_example.parquet"
-# -
 
+# %%
 # setup a source cloudpath using unsigned (anonymous) requests to AWS S3
+# to access publicly-available data using CytoTable
 source_cloud_path = S3Client(no_sign_request=True).CloudPath(source_path)
 source_cloud_path
 
+# %%
 # remove the dest_path if it's present
 if pathlib.Path(dest_path).is_file():
     pathlib.Path(dest_path).unlink()
 
+# %%
 # show the files we will use as source data with CytoTable
 list(source_cloud_path.glob("*"))
 
-# +
+# %%
 # %%time
 
 # run cytotable convert
@@ -64,21 +68,25 @@ result = cytotable.convert(
     no_sign_request=True,
 )
 print(pathlib.Path(result).name)
-# -
 
+# %%
 # show the table head using pandas
 pq.read_table(source=result).to_pandas().head()
 
+# %%
 # show metadata for the result file
 pq.read_metadata(result)
 
+# %%
 # show schema metadata which includes CytoTable information
 # note: this information will travel with the file.
 pq.read_schema(result).metadata
 
+# %%
 # show schema column name summaries
 print("Column name prefix counts:")
 dict(Counter(w.split("_", 1)[0] for w in pq.read_schema(result).names))
 
+# %%
 # show full schema details
 pq.read_schema(result)
--- docs/source/examples/cytotable_mise_en_place_general_overview.py
+++ docs/source/examples/cytotable_mise_en_place_general_overview.py
@@ -3,8 +3,8 @@
 #   jupytext:
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
+#       format_name: percent
+#       format_version: '1.3'
 #       jupytext_version: 1.17.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
@@ -12,9 +12,11 @@
 #     name: python3
 # ---
 
+# %% [markdown]
 # # CytoTable mise en place (general overview)
 #
-# This notebook includes a quick demonstration of CytoTable to help you understand the basics of using the package and the biological basis of each step.
+# This notebook includes a quick demonstration of a common CytoTable workflow.
+# This notebook wil help you understand the basics of using CytoTable and the biological basis of each step.
 # We provide a high-level overview of the related concepts to give greater context about where and how the data are changed in order to gain new insights.
 #
 # The name of the notebook comes from the french _mise en place_:
@@ -23,7 +25,7 @@
 # > refer to organizing and arranging the ingredients ..."
 # > - [Wikipedia](https://en.wikipedia.org/wiki/Mise_en_place)
 
-# +
+# %%
 import pathlib
 from collections import Counter
 
@@ -37,28 +39,31 @@ import cytotable
 # setup variables for use throughout the notebook
 source_path = "../../../tests/data/cellprofiler/examplehuman"
 dest_path = "./example.parquet"
-# -
 
+# %%
 # remove the dest_path if it's present
 if pathlib.Path(dest_path).is_file():
     pathlib.Path(dest_path).unlink()
 
+# %%
 # show the files we will use as source data with CytoTable
 list(pathlib.Path(source_path).glob("*"))
 
-# ## Phase 1: Cells are stained and images are captured by microscopes
+# %% [markdown]
+# ## Phase 1: Cells are imaged by microscopes, with optional fluorescence staining
 #
+# ![Image showing cells being stained and captured as images using a microscope.](../_static/cell_to_image.png)
 #
+# __Figure 1.__ _A microscope images cells to highlight cell processes. Often, fluorescence dyes paint the cells to mark specific proteins, compartments, and/or organelles._
 #
-# __Figure 1.__ _Cells are stained in order to highlight cellular compartments and organelles. Microscopes are used to observe and capture data for later use._
+# CytoTable processes microscopy-based data that are created from multiple upstream steps.
+# CytoTable does not require any specific sample preparation, and can work with any microscopy experimental design.
+# However, most often, CytoTable processes fluorescence microscopy images from the Cell Painting assay.
+# In the Cell Painting assay, scientists stain cells with a panel of six fluorescent dyes that mark key cellular compartments and organelles, including the nucleus, nucleoli/RNA, endoplasmic reticulum, mitochondria, actin cytoskeleton, Golgi apparatus, and plasma membrane ([Bray et al., 2016](https://doi.org/10.1038/nprot.2016.105); [Gustafsdottir et al., 2013](https://doi.org/10.1371/journal.pone.0080999)). Scientists then use microscopes to image these cells across fluorescence channels, and use image analysis software to produce high-content morphology profiles of individual cells for downstream analysis .
 #
-# CytoTable uses data created from multiple upstream steps involving images of
-# stained biological objects (typically cells).
-# Cells are cultured in multi-well plates, perturbed, and then fixed before being stained with a panel of six fluorescent dyes that highlight key cellular compartments and organelles, including the nucleus, nucleoli/RNA, endoplasmic reticulum, mitochondria, actin cytoskeleton, Golgi apparatus, and plasma membrane. These multiplexed stains are imaged across fluorescence channels using automated high-content microscopy, producing rich images that capture the morphology of individual cells for downstream analysis ([Bray et al., 2016](https://doi.org/10.1038/nprot.2016.105); [Gustafsdottir et al., 2013](https://doi.org/10.1371/journal.pone.0080999)).
-#
-# We use the ExampleHuman dataset provided from CellProfiler Examples ([Moffat et al., 2006](https://doi.org/10.1016/j.cell.2006.01.040), [CellProfiler Examples Link](https://github.com/CellProfiler/examples/tree/master/ExampleHuman)) to help describe this process below.
+# We use the ExampleHuman dataset provided from CellProfiler Examples ([Moffat et al., 2006](https://doi.org/10.1016/j.cell.2006.01.040), [CellProfiler Examples Link](https://github.com/CellProfiler/examples/tree/master/ExampleHuman)) to describe this process below.
 
-# +
+# %%
 # display the images we will gather features from
 image_name_map = {"d0.tif": "DNA", "d1.tif": "PH3", "d2.tif": "Cells"}
 
@@ -69,39 +74,43 @@ for image in pathlib.Path(source_path).glob("*.tif"):
             stain = val
     print(f"\nImage with stain: {stain}")
     display(Image.open(image))
-# -
 
-# ## Phase 2: Images are segmented to build numeric feature datasets via CellProfiler
+# %% [markdown]
+# ## Phase 2: CellProfiler segments cells and measures numeric features
 #
-# ![image.png](attachment:3d765d50-3faa-4756-9a8b-965af1b07855.png)
+# ![Image showing CellProfiler being used to create image segmentations, measurements, and exporting numeric feature data to a file.](../_static/image_to_features.png)
 #
-# __Figure 2.__ _CellProfiler is configured to use images and performs segmentation to evaluate numeric representations of cells. This data is captured for later use in tabular file formats such as CSV or SQLite tables._
 #
-# After acquisition, the multiplexed images are processed using image-analysis software such as CellProfiler, which segments cells and their compartments into distinct regions of interest. From these segmented images, hundreds to thousands of quantitative features are extracted per cell, capturing properties such as size, shape, intensity, texture, and spatial organization.
+# __Figure 2.__ _CellProfiler takes in microscopy images and performs single-cell segmentation to distinguish cells from background. CellProfiler then measures "hand-engineered" computer vision features from every single cell. These data are captured for later use in a CSV table or SQLite database._
+#
+# After acquisition, scientists process the images using image-analysis software such as CellProfiler. CellProfiler segments single cells and their biological compartments into distinct regions of interest. From these segmented cells, CellProfiler extracts hundreds to thousands of quantitative features per cell, capturing properties such as size, shape, intensity, texture, and spatial organization.
 # These high-dimensional feature datasets provide a numerical representation of cell morphology that serves as the foundation for downstream profiling and analysis ([Carpenter et al., 2006](https://doi.org/10.1186/gb-2006-7-10-r100)).
 #
-# CellProfiler was used in conjunction with the `.cppipe` file to produce the following images and data tables from the ExampleHuman dataset.
+# We use CellProfiler (with a prespecified configuration `.cppipe` file) to produce the following images and data tables from the ExampleHuman dataset.
 
+# %%
 # show the segmentations through an overlay with outlines
 for image in pathlib.Path(source_path).glob("*Overlay.png"):
     print(f"Image outlines from segmentation (composite)")
     print("Color key: (dark blue: nuclei, light blue: cells, yellow: PH3)")
     display(Image.open(image))
 
-# show the tables generated  from the resulting CSV files
+# %%
+# show the tables generated from the resulting CSV files
 for profiles in pathlib.Path(source_path).glob("*.csv"):
     print(f"\nProfiles from CellProfiler: {profiles}")
     display(pd.read_csv(profiles).head())
 
-# ## Phase 3: Numeric feature datasets from CellProfiler are harmonized by CytoTable
+# %% [markdown]
+# ## Phase 3: CytoTable harmonizes the feature datasets that CellProfiler generates
 #
-# ![image.png](attachment:687ab1fc-f587-45ea-a749-ff848bf55ff6.png)
+# ![Image showing feature data being read by CytoTable and exported to a CytoTable file.](../_static/features_to_cytotable.png)
 #
-# The high-dimensional feature tables produced by CellProfiler often vary in format depending on the imaging pipeline, experiment, or storage system. CytoTable standardizes these single-cell morphology datasets by harmonizing outputs into consistent, analysis-ready formats such as Parquet or AnnData. This unification ensures that data from diverse experiments can be readily integrated and processed by downstream profiling tools like Pycytominer or coSMicQC, enabling scalable and reproducible cytomining workflows.
+# CellProfiler produces high-dimensional feature tables that vary in format depending on the imaging pipeline, experiment, or storage system. Sometimes these feature tables are thousands of columns and hundreds of thousands of rows. CytoTable harmonizes these outputs into consistent, analysis-ready formats such as Parquet or AnnData. This unification ensures that data from diverse experiments can be readily integrated and processed by downstream profiling tools like Pycytominer or coSMicQC, enabling scalable and reproducible bioinformatics workflows.
 #
-# We use CytoTable below to process the numeric feature data observed above.
+# We use CytoTable below to process the numeric feature data we generated above.
 
-# +
+# %%
 # %%time
 
 # run cytotable convert
@@ -114,21 +123,25 @@ result = cytotable.convert(
     preset="cellprofiler_csv",
 )
 print(pathlib.Path(result).name)
-# -
 
+# %%
 # show the table head using pandas
 pq.read_table(source=result).to_pandas().head()
 
+# %%
 # show metadata for the result file
 pq.read_metadata(result)
 
+# %%
 # show schema metadata which includes CytoTable information
 # note: this information will travel with the file.
 pq.read_schema(result).metadata
 
+# %%
 # show schema column name summaries
 print("Column name prefix counts:")
 dict(Counter(w.split("_", 1)[0] for w in pq.read_schema(result).names))
 
+# %%
 # show full schema details
 pq.read_schema(result)
