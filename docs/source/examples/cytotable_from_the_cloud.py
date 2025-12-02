0e875d34b99996024f68efe9086c891ac8101347
Dave Bunten <dave.bunten@cuanschutz.edu>
2025-12-02 10:39:31 -0700

    Cloud-based CSV example notebook additions (#393)
    * update cloud-based notebook for csv's
    
    * fix cloudpath sqlite bug
    
    * rerun cloud notebook
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * add details about the chunk size
    
    Co-Authored-By: Jenna Tomkinson <107513215+jenna-tomkinson@users.noreply.github.com>
    
    * add comment about compartments
    
    Co-Authored-By: Jenna Tomkinson <107513215+jenna-tomkinson@users.noreply.github.com>
    
    * add comments about presets
    
    Co-Authored-By: Jenna Tomkinson <107513215+jenna-tomkinson@users.noreply.github.com>
    
    * linting
    
    * [pre-commit.ci lite] apply automatic fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>
    Co-authored-by: Jenna Tomkinson <107513215+jenna-tomkinson@users.noreply.github.com>

--- docs/source/examples/cytotable_from_the_cloud.py
+++ docs/source/examples/cytotable_from_the_cloud.py
@@ -3,16 +3,15 @@
 #   jupytext:
 #     text_representation:
 #       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.17.2
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.18.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
-# %% [markdown]
 # # CytoTable from the cloud (using cloud-based data sources)
 #
 # ![Image showing feature data from the cloud being read by CytoTable and exported to a CytoTable file afterwards.](../_static/features_to_cytotable_cloud.png)
@@ -22,43 +21,50 @@
 # This notebook includes a quick demonstration of CytoTable with cloud-based data sources.
 # For a more general overview of using CytoTable and the concepts behind the work please see: [CytoTable mise en place (general overview)](https://cytomining.github.io/CytoTable/examples/cytotable_mise_en_place_general_overview.html)
 
-# %%
+# +
 import pathlib
 from collections import Counter
 
 import pandas as pd
 import pyarrow.parquet as pq
 from cloudpathlib import CloudPath, S3Client
-from IPython.display import Image, display
-from PIL import Image
 
 import cytotable
 
-# setup variables for use throughout the notebook
-source_path = "s3://cellpainting-gallery/cpg0000-jump-pilot/source_4/workspace/analysis/2020_11_04_CPJUMP1/BR00116991/analysis/BR00116991-A01-1"
+# -
+
+# ## Using CytoTable with cloud-based CSV's
+
+# setup variables for use in this section of the notebook
+source_path = (
+    "s3://cellpainting-gallery/cpg0037-oasis/broad/workspace/"
+    "analysis/2025_01_12_U2OS_Batch2/BR00146052/analysis/BR00146052-A01-1/"
+)
 dest_path = "./cloud_example.parquet"
 
-# %%
-# setup a source cloudpath using unsigned (anonymous) requests to AWS S3
-# to access publicly-available data using CytoTable
-source_cloud_path = S3Client(no_sign_request=True).CloudPath(source_path)
-source_cloud_path
-
-# %%
 # remove the dest_path if it's present
 if pathlib.Path(dest_path).is_file():
     pathlib.Path(dest_path).unlink()
 
-# %%
-# show the files we will use as source data with CytoTable
+# setup a source cloudpath using unsigned (anonymous) requests to AWS S3
+# to access publicly-available data using CytoTable
+source_cloud_path = S3Client(no_sign_request=True).CloudPath(source_path)
+print(source_cloud_path)
+# show the files within the path
 list(source_cloud_path.glob("*"))
 
-# %%
+# +
 # %%time
 
 # run cytotable convert
 result = cytotable.convert(
     source_path=source_path,
+    source_datatype="csv",
+    # Set the chunk size for paginated processing.
+    # Smaller values use fewer resources but take longer;
+    # larger values use more resources and process faster.
+    chunk_size=30000,
+    # specify the destination path
     dest_path=dest_path,
     # specify a destination data format type
     dest_datatype="parquet",
@@ -68,25 +74,87 @@ result = cytotable.convert(
     no_sign_request=True,
 )
 print(pathlib.Path(result).name)
+# -
 
-# %%
 # show the table head using pandas
 pq.read_table(source=result).to_pandas().head()
 
-# %%
 # show metadata for the result file
 pq.read_metadata(result)
 
-# %%
 # show schema metadata which includes CytoTable information
 # note: this information will travel with the file.
 pq.read_schema(result).metadata
 
-# %%
 # show schema column name summaries
+# note: we have all compartments in one table
+# after processing with CytoTable.
 print("Column name prefix counts:")
 dict(Counter(w.split("_", 1)[0] for w in pq.read_schema(result).names))
 
-# %%
+# ## Using CytoTable with cloud-based SQLite databases
+
+# setup variables for use in this section of the notebook
+source_path = (
+    "s3://cellpainting-gallery/cpg0016-jump/source_4/"
+    "workspace/backend/2021_08_23_Batch12/BR00126114"
+)
+dest_path = "./cloud_example.parquet"
+
+# remove the dest_path if it's present
+if pathlib.Path(dest_path).is_file():
+    pathlib.Path(dest_path).unlink()
+
+# setup a source cloudpath using unsigned (anonymous) requests to AWS S3
+# to access publicly-available data using CytoTable
+source_cloud_path = S3Client(no_sign_request=True).CloudPath(source_path)
+print(source_cloud_path)
+# show the files within the path
+list(source_cloud_path.glob("*"))
+
+# +
+# %%time
+
+# run cytotable convert
+result = cytotable.convert(
+    source_path=source_path,
+    source_datatype="sqlite",
+    # Set the chunk size for paginated processing.
+    # Smaller values use fewer resources but take longer;
+    # larger values use more resources and process faster.
+    chunk_size=30000,
+    # specify the destination path
+    dest_path=dest_path,
+    # specify a destination data format type
+    dest_datatype="parquet",
+    # specify a preset which enables quick use of common input file formats
+    # note: presets can be customized to meet the needs of
+    # specific datasets. Check out the presets.py module for more here.
+    preset="cellprofiler_sqlite_cpg0016_jump",
+    # use unsigned (anonymous) requests to AWS S3
+    no_sign_request=True,
+    # set a local cache to avoid challenges with
+    # OS-specific temp disk space
+    local_cache_dir=f"./sqlite_s3_cache",
+)
+print(pathlib.Path(result).name)
+# -
+
+# show the table head using pandas
+pq.read_table(source=result).to_pandas().head()
+
+# show metadata for the result file
+pq.read_metadata(result)
+
+# show schema metadata which includes CytoTable information
+# note: this information will travel with the file.
+pq.read_schema(result).metadata
+
+# show schema column name summaries
+# note: we have all compartments in one table
+# after processing with CytoTable.
+print("Column name prefix counts:")
+dict(Counter(w.split("_", 1)[0] for w in pq.read_schema(result).names))
+
 # show full schema details
 pq.read_schema(result)
